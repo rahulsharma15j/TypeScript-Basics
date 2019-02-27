7cdcdab1e85d4be6730056574d0a1226e997f7a7
Rahul Sharma <rahulsharma15j@gmail.com>
2019-02-27 18:39:19 +0530

    data types

--- main.ts
+++ main.ts
@@ -1,5 +1,34 @@
 //this is a typescript file.
 
-let myFirstVariable:String = "hello world";
+//declaring a String data type.
+let someName : string = "rahul";
+let someAddress : string = "india";
+let someBigParagraph : string = 
+     `The Hypertext Transfer Protocol (HTTP) is an application protocol for distribute`
+     "Hypertext is structured text that uses logical links (hyperlinks) between nodes";
 
-console.log(myFirstVariable);
+//you can declare numbers
+let skillScoreOfStudent : number = 10;
+let totalNumberOfUsers : number = 239999987645;
+let userRating : number = 49.23242323;
+
+//you can declare booleans
+let isRegisteredUser : boolean = true;
+let isPaidUser : boolean = true;
+
+//you can declare arrays
+let popularSports : string[] = ["soccer", "basketball", "cricket"];
+
+let someNumberArrays : number[] = [1, 2, 3, 4, 5, 6];
+
+//you can define tuples which are basically array's whose length and value types are known
+//declare a tuple type
+let x : [ string, number];
+
+//Initialize it
+x = [ "hello", 10 ]; //ok
+
+//Initialize it incorrectly
+//x = [ 10, "hello"]; //Error
+
+//
